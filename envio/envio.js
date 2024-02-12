77b41ad66adda858634a248a38bbe4b3b1b8e462
RefinedCandle49 <valdezvargaskevin@gmail.com>
2024-02-12 18:05:00 -0500

    El envio se guarda y también se lista

--- envio/envio.js
+++ envio/envio.js
@@ -1,29 +1,91 @@
-$(document).ready(function() {
+$.getScript('auth.js', function() {
 
-    // Evento para agregar un nuevo detalle del paquete
-    
-
-    // Función para obtener el token de autenticación
-    function obtenerToken() {
-        return new Promise(function(resolve, reject) {
-            $.ajax({
-                url: "http://localhost:8080/usuario/autenticar",
-                method: "POST",
-                data: JSON.stringify({
-                    username: "Kevin",
-                    password: "12345"
-                }),
-                contentType: "application/json",
-                
-                success: function (token) {
-                    resolve(token);
-                },
-                error: function (error) {
-                    reject(error);
-                }
+    function consultarEnvio() {
+        $('#consultaForm').submit(function (event) {
+            event.preventDefault();
+            var idEnvio = $('#idEnvio').val();
+            obtenerToken().then(function(token) {
+                $.ajax({
+                    url: "http://localhost:8080/envio/listar/" + idEnvio,
+                    method: "GET",
+                    headers: {
+                        "Authorization": "Bearer " + token
+                    },
+                    success: function (datos) {
+                        mostrarDatosEnvio(datos[0]);
+                    }
+                });
             });
         });
     }
+
+    function mostrarDatosEnvio(envio) {
+        var tabla = "<table>";
+
+        tabla += "<tr><td>Número de rastreo</td><td><input type='text' value='" + envio.idGuia + "'></td></tr>";
+        tabla += "<tr><td>Origen</td><td><input type='text' value='" + envio.origen + "'></td></tr>";
+        tabla += "<tr><td>Destino</td><td><input type='text' value='" + envio.destino + "'></td></tr>";
+        tabla += "<tr><td>Estado</td><td><input type='text' value='" + envio.estado + "'></td></tr>";
+        tabla += "<tr><td>Observaciones</td><td><input type='text' value='" + envio.observaciones + "'></td></tr>";
+        tabla += "<tr><td>ID Remitente</td><td><input type='text' value='" + envio.remitente.idRemitente + "'></td></tr>";
+        tabla += "<tr><td>Remitente</td><td><input type='text' value='" + envio.remitente.nombreRemitente + "'></td></tr>";
+        tabla += "<tr><td>DNI Remitente</td><td><input type='text' value='" + envio.remitente.dniRemitente + "'></td></tr>";
+        tabla += "<tr><td>Teléfono Remitente</td><td><input type='text' value='" + envio.remitente.telefono + "'></td></tr>";
+        tabla += "<tr><td>ID Destinatario</td><td><input type='text' value='" + envio.destinatario.idDestinatario + "'></td></tr>";
+        tabla += "<tr><td>Destinatario</td><td><input type='text' value='" + envio.destinatario.nombreDestinatario + "'></td></tr>";
+        tabla += "<tr><td>Dirección Destinatario</td><td><input type='text' value='" + envio.destinatario.direccion + "'></td></tr>";
+        tabla += "<tr><td>Teléfono Destinatario</td><td><input type='text' value='" + envio.destinatario.telefono + "'></td></tr>";
+        tabla += "<tr><td>ID Paquete</td><td><input type='text' value='" + envio.paquete.idPaquete + "'></td></tr>";
+        tabla += "<tr><td>Peso del Paquete</td><td><input type='text' value='" + envio.paquete.peso + "'></td></tr>";
+        tabla += "<tr><td>Medidas del Paquete</td><td><input type='text' value='" + envio.paquete.medidas + "'></td></tr>";
+
+        $('#contenedor-envio').html(tabla);
+    }
+
+    $(document).ready(function () {
+        consultarEnvio();
+    });
+
+    function consultarDetalles() {
+        $('#consultaForm').submit(function (event) {
+            event.preventDefault();
+            var idEnvio = $('#idEnvio').val();
+            obtenerToken().then(function(token) {
+                $.ajax({
+                    url: "http://localhost:8080/detallepaquete/listar/" + idEnvio,
+                    method: "GET",
+                    headers: {
+                        "Authorization": "Bearer " + token
+                    },
+                    success: function (datos) {
+                        mostrarDatosDetalle(datos);
+                    }
+                });
+            });
+        });
+    }
+
+    function mostrarDatosDetalle(detallesPaquete) {
+        var tabla = "<table id='tablaDatos'>";
+        tabla += "<tr><th>Descripción</th><th>Cantidad</th></tr>";
+
+        for (var i =  0; i < detallesPaquete.length; i++) {
+            var detalle = detallesPaquete[i];
+            tabla += "<tr>";
+            tabla += "<td>" + detalle.descripcion + "</td>";
+            tabla += "<td>" + detalle.cantidad + "</td>";
+            tabla += "</tr>";
+        }
+
+        tabla += "</table>";
+        $('#contenedor-detalle').html(tabla);
+    }
+
+    $(document).ready(function () {
+        consultarDetalles();
+    });
+
+
 
     // Función para crear el envío
     function crearEnvio() {
